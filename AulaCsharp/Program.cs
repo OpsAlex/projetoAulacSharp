0ec6b4ff484a25e95bbdbd45f2bfee73130c4fd3
Alex Bezerra <alexbezerra50@hotmail.com>
2021-07-01 19:28:05 -0300

    Delegates

---
+++ AulaCsharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AulaCsharp.Services;
+
+namespace AulaCsharp
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            // Aula Delegates
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Max;
+
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
