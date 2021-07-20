fd01ecfe127463a10d872c7f9e2cf3be3d6e9f89
Alex Bezerra <alexbezerra50@hotmail.com>
2021-07-19 21:23:14 -0300

    Aula LINQ testandoComandos

--- AulaCsharp/Entities/Product.cs
+++ AulaCsharp/Entities/Product.cs
@@ -1,25 +1,42 @@
-﻿
+﻿using System.Globalization;
+
 namespace AulaCsharp.Entities
 {
     class Product
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+        public Category Category { get; set; }
 
-        public Product(string name, double price)
+        
+        /*
+        public Product(int id, string name, double price, Category category)
         {
             Name = name;
             Price = price;
         }
+        */
+
 
         public override string ToString()
         {
+            return Id + ", " + Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture) + ", " + Category.Name
+                + ", " + Category.Tier;
+        }
+        /*public override string ToString()
+        {
             return Name + ", " + Price.ToString("F2");
         }
+        */
 
+
+        /*
         public static bool ProductTest(Product p)
         {
             return p.Price >= 100.0;
         }
+        */
+        
     }
 }
