4e0e36e4d252363c93294fab44d16b187d4d3c67
Alex Bezerra <alexbezerra50@hotmail.com>
2021-07-21 18:34:08 -0300

    Realizando os comandos como SQL

--- AulaCsharp/Program.cs
+++ AulaCsharp/Program.cs
@@ -41,19 +41,24 @@ namespace AulaCsharp
 
 
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            // var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            var r1 = from p in products where p.Category.Tier == 1 && p.Price < 900.0 select p;
             Print("TIER 1 AND PRICE < 900:", r1);
 
-            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            // var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 = from p in products where p.Category.Name == "Tools" select p.Name;
             Print("NAMES OF PRODUCTS FROM TOOLS", r2);
 
-            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            // var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            var r3 = from p in products where p.Name[0] == 'C' select new { p.Name, p.Price, CategoryName = p.Category.Name };
             Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
 
-            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            // var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 = from p in products where p.Category.Tier == 1 orderby p.Name orderby p.Price select p;
             Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
 
-            var r5 = r4.Skip(2).Take(4);
+            // Consulta Estilo SQL
+            var r5 = (from p in r4 select p).Skip(2).Take(4);
             Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
 
             var r6 = products.FirstOrDefault();
@@ -82,7 +87,8 @@ namespace AulaCsharp
             Console.WriteLine("Category 1 aggregate sum: " + r15);
             Console.WriteLine();
 
-            var r16 = products.GroupBy(p => p.Category);
+            // var r16 = products.GroupBy(p => p.Category);
+            var r16 = from p in products group p by p.Category;
             foreach (IGrouping<Category, Product> group in r16)
             {
                 Console.WriteLine("Category " + group.Key.Name + ":");
@@ -93,7 +99,7 @@ namespace AulaCsharp
                 Console.WriteLine();
             }
 
-
+            Console.WriteLine("__________________________________\n");
 
             // Especificar o Data Source
             int[] numbers = new int[] { 2, 3, 4, 5, 6 };
